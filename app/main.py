9309a615e174a8e7b21137d243851ee2fea411d1
sionkim <skim14@academic.rrc.ca>
2025-05-28 22:58:52 -0500

    Update url

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="TradeMo API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://fastapi.trademo.boutique"],
+    allow_origins=["https://trademo.boutique"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
